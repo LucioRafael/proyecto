b1d52c7bf579a30924324513c9d598461a48b65e
LucioRafael <20170019@uttcampus.edu.mx>
2024-04-07 12:08:02 -0600

    vistass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,10 @@
-<h1>HOLA MUNDO!!</h1>
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    
+  <h1 class="text-5xl text-center pt-24">Welcome to my application</h1>
+
+
+@endsection
